185a29c1d1d7dc4e86338a54cad7109ad410b2d8
Giap Nguyen <zap@vl965-172-31-253-5.wireless.umass.edu>
2024-04-24 17:07:42 -0400

    add community add trading view

---
+++ src/client/milestone-02/tradingView/index.js
@@ -0,0 +1,27 @@
+new TradingView.widget(
+    {
+        "autosize": true,
+        "symbol": "NASDAQ:NVDA",
+        "interval": "240",
+        "timezzone": "Etc/Utc",
+        "theme": "dark",
+        "style": "1",
+        "locale": "en",
+        "toolbar_bg": "#f1f3f6",
+        "enable_publishing": false,
+        "withdateranges": true,
+        "hide_side_toolbar": false,
+        "allow_symbol_change": true,
+        "details": true,
+        "hotlist": true,
+        "calendar": true,
+        "studies": [
+            "STD;SMA"
+        ],
+        "container_id": "chart",
+        "show_popup_button": true,
+        "popup_width": "1000",
+        "popup_height": "650",
+        "support_host": "https://www.tradingview.com"
+    }
+);
